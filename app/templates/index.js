dca2773bc37647dbc9592e056cd727941d67cee7
Scholl, Nathan (P3111462) <c-nathan.scholl@charter.com>
2022-09-29 23:26:57 -0600

    Fuck

--- app/templates/index.js
+++
@@ -1,9 +0,0 @@
-ej.base.enableRipple(true);
-
-//To change the Button type.
-var button = new ej.buttons.Button({cssClass: 'e-link'});
-button.appendTo('#element');
-
-button.element.onclick = function () {
-    window.open("http://http://54.227.208.165:56733/work_history");
-}
