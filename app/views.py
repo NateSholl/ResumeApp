602307dd8568c7a88988d42988c5aa9bab583e01
Scholl, Nathan (P3111462) <c-nathan.scholl@charter.com>
2022-09-30 09:40:46 -0600

    changes

--- app/views.py
+++ app/views.py
@@ -5,14 +5,14 @@ from app import app
 def home():
     return render_template('index.html')
     
-@app.route('/work_history')
-def template():
-    return render_template('work_history.html')
+# @app.route('/work_history')
+# def template():
+#     return render_template('work_history.html')
 
-@app.route('/contact')
-def template():
-    return render_template('contact.html')
+# @app.route('/contact')
+# def template():
+#     return render_template('contact.html')
 
-@app.route('/experiments')
-def template():
-    return render_template('experiments.html')
+# @app.route('/experiments')
+# def template():
+#     return render_template('experiments.html')
