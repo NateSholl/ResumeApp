1ebec496634490d08466fd5fab1b2540359a5738
Scholl, Nathan (P3111462) <c-nathan.scholl@charter.com>
2022-10-03 09:36:12 -0600

    Changes

--- app/static/synth.js
+++ app/static/synth.js
@@ -1,15 +1,15 @@
-const audioContext = new AudioContext();
-const oscList = [];
-let mainGainNode = null;
+// const audioContext = new AudioContext();
+// const oscList = [];
+// let mainGainNode = null;
 
-const keyboard = document.querySelector(".keyboard");
-const wavePicker = document.querySelector("select[name='waveform']");
-const volumeControl = document.querySelector("input[name='volume']");
+// const keyboard = document.querySelector(".keyboard");
+// const wavePicker = document.querySelector("select[name='waveform']");
+// const volumeControl = document.querySelector("input[name='volume']");
 
-let noteFreq = null;
-let customWaveform = null;
-let sineTerms = null;
-let cosineTerms = null;
+// let noteFreq = null;
+// let customWaveform = null;
+// let sineTerms = null;
+// let cosineTerms = null;
 
 function test(){
   console.log('testing');
